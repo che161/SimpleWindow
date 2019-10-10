fa1f0aab744c918a794f32119cbac55aa66545b9
Chen <che161@csiro.au>
2019-10-10 12:17:23 +1100

    Plot average star difference

--- src/Plots.R
+++ src/Plots.R
@@ -12,6 +12,10 @@ diff
 number_dwellings <- allstate %>% group_by(State) %>% summarise(DwellingNo = n() )
 number_dwellings
 
+stardiff <- group_by(allstate, State, StarDiff) %>% summarise(DwellingNo = n() )
+stardiff
+averagediff <- group_by(abs_allstate, State) %>% summarise(MeanDiff = mean(AbsDiff) )
+averagediff
 # Using ggplot2
 ggplot(
   data = number_dwellings, 
@@ -19,19 +23,38 @@ ggplot(
                 col.lab="red", cex.axis = 3, cex.lab = 4)
 ) +
   geom_col(width = 0.7) +   geom_col(fill = "green", width = 0.75) +
-  geom_text() +
+  geom_text(aes(label = DwellingNo), colour ="blue", vjust = -0.5) +
   labs(title = "Figure 1",
        x = "State",
        y = "Number of Dwellings"
-  )
+  ) +
+  theme(axis.title = element_text(colour = "red", face = "bold", size = 18))
+  
 
 ggplot(
-  data = number_dwellings, 
-  mapping = aes(x = State, y = DwellingNo, fill = DwellingNo, label = DwellingNo )
+  data = stardiff, 
+  mapping = aes(x = StarDiff, y = DwellingNo, group = State, label = DwellingNo )
+) +
+  geom_col(fill = "green",width = 0.05) +   
+  geom_text(aes(label = DwellingNo), colour = "blue", vjust = -0.15) +
+  labs(title = "Figure 2",
+       x = "Star Rating Difference",
+       y = "Number of Dwellings"
+  ) +
+  theme(axis.title = element_text(colour = "red", face = "bold", size = 18))+
+    facet_wrap( ~ State)
+
+ggplot(
+  data = averagediff, 
+  mapping = aes(x = State, y = MeanDiff, label = sprintf("%.02f %%", MeanDiff),
+                col.lab="red", cex.axis = 3, cex.lab = 4)
 ) +
   geom_col(width = 0.7) +   geom_col(fill = "green", width = 0.75) +
-  geom_text() +
-  labs(title = "Figure 1",
+  geom_text(aes(label = sprintf("%.02f", MeanDiff)), colour ="blue", vjust = -0.5) +
+  labs(title = "Figure 3",
        x = "State",
-       y = "Number of Dwellings"
-  )
+       y = "Mean Star Rating Difference"
+  ) +
+  theme(axis.title = element_text(colour = "red", face = "bold", size = 18))
+
+
